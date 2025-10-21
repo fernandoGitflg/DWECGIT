2a9f9b6abdadbcbf0987b098089bbff86167739e
fernandoGitflg <feymarlopga@gmail.com>
2025-10-21 12:21:26 +0200

    Cambiado

--- Practica1_Lopez_Garrido_Fernando/Lopez_Garrido_Fernando_Ejercicio2.js
+++ Practica1_Lopez_Garrido_Fernando/Lopez_Garrido_Fernando_Ejercicio2.js
@@ -1,17 +1,25 @@
 let texto = "En un lugar de la mancha";
-let f4 = frase =>{
-    return frase.split(" ")
-    .toString()
-    .replace(/,/g, "")
-    .toLowerCase();
-}
+let f4 = frase => {
+    return frase
+        .split(" ")
+        .map((palabra, i) =>
+            i === 0
+                ? palabra.toLowerCase()
+                : palabra[0].toUpperCase() + palabra.slice(1).toLowerCase()
+        )
+        .toString()
+        .replace(/,/g, "");
+};
 console.log("Funcionf4:");
 console.log(f4(texto));
 
 let f5 = frase =>{
-    return (frase.split(" ")
+    let totalLetras= frase.split(" ")
     .map((palabra,i,arr) =>arr[i].length)
-    .reduce((ac,n,i,arr)=>ac+n));//Falta dividir por el array length despues de sumarlo todo
+    .reduce((ac,n,i,arr)=>ac+n);
+    let totalPalabras= frase.split(" ").length
+
+    return totalLetras/totalPalabras;
 }
 console.log("Funcionf5:");
 console.log(f5(texto));
