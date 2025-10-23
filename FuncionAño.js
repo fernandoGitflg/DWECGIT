8e91e8fe1e9ae71b382dc5848af63b16da786ab0
fernandoGitflg <feymarlopga@gmail.com>
2025-10-23 10:01:01 +0200

    EjercicioPractica2

--- FuncionAño.js
+++ FuncionAño.js
@@ -2,10 +2,16 @@ let siglos=["I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII"
 let funcionAño = año =>{
      return "Es el siglo "+siglos[Math.ceil(año/100)-1];
 }
-console.log(funcionAño(Math.floor(Math.random() * 2023.99)));
 
+for(let i=0;i<=100;i++){
+console.log(funcionAño(Math.floor(Math.random() * 2023.99)));
+}
 
 let contadorDias=(fechaCumple)=>{
-    console.log("Te quedan "+Math.ceil((Date.parse(fechaCumple)-Date.now())/(1000*3600*24))+" dias para tu cumpleaños");
+    console.log("Te quedan "+
+    (Math.floor((Date.parse(fechaCumple)-Date.now())/(1000*3600*24))
+    +" dias para tu cumpleaños y "+
+    Math.round(((Date.parse(fechaCumple)-Date.now()))/(1000*3600*24)/(1000 * 60 * 60))
+    +" horas"));
 } 
 contadorDias("2026-01-28");
