748b9fea84e1e9de9573decd1ae17d8b29e603d4
fernandoGitflg <feymarlopga@gmail.com>
2025-10-28 19:25:47 +0100

    tabla

--- Repaso/inicializarArray.js
+++ Repaso/inicializarArray.js
@@ -22,21 +22,44 @@ console.log(diasLaborales);
 diasLaborales.splice(0,2);
 console.log(diasLaborales);*/
 
-let sedes=["","Ayuntamiento","Polideportivo","Instituto","Mercado","Colegio"];
-let partido=["PV","OV","VpSI","UPV"];
-let tabla=[[],[]];
- 
+let sedes = ["", "Ayuntamiento", "Polideportivo", "Instituto", "Mercado", "Colegio"];
+let partido = ["PV", "OV", "VpSI", "UPV"];
+let tabla = [[], []];
+
 let generarVotos = () => {
-    return Math.floor(Math.random() * 5.99) + 5;
+  return Math.floor(Math.random() * 5.99) + 5;
 };
 
-tabla[0]=partido;
+tabla[0] = partido;
 for (let i = 0; i < partido.length; i++) {
-    let votosPorSede = [];
-    for (let j = 0; j < sedes.length; j++) {
-      votosPorSede.push(generarVotos());
-    }
-    tabla[1].push(votosPorSede);
+  let votosPorSede = [];
+  for (let j = 0; j < sedes.length; j++) {
+    votosPorSede.push(generarVotos());
   }
+  tabla[1].push(votosPorSede);
+}
 
-//Acabar este y ordenar el recuento de votos por partido d forma ordenada con el sort
+console.log("Tabla de votos:");
+console.log(" ", sedes.slice(1).join(" | "));
+
+for (let i = 0; i < partido.length; i++) {
+  let fila = tabla[1][i].slice(1).join(" | ");
+  console.log(partido[i] + fila);
+}
+let totales = [];
+for (let i = 0; i < partido.length; i++) {
+  let suma = 0;
+  for (let j = 0; j < sedes.length; j++) {
+    suma += tabla[1][i][j];
+  }
+  totales.push(suma);
+}
+let indicesOrdenados = totales
+  .map((votos, index) => index)
+  .sort((a, b) => totales[b] - totales[a]);
+
+console.log("Recuento de votos por partido (ordenado):");
+for (let i = 0; i < indicesOrdenados.length; i++) {
+  let idx = indicesOrdenados[i];
+  console.log(partido[idx] + ": " + totales[idx] + " votos");
+}
